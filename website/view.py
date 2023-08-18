c9e354dcb1f840e88e2966d824efc655430c2c35
Saif Rahman <saif63151@gmail.com>
2023-08-18 19:30:44 +0530

    implemented a second model for search using googleNews model and using word embeddings

--- website/view.py
+++ website/view.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request
-from .search import product_search
+from .search import product_search, word2vec_search
 view = Blueprint('view', __name__)
 
 
@@ -12,7 +12,7 @@ def search():
     user_query = request.args.get('q')  # Assuming the query is passed as a query parameter
     # products = get_product_attributes()  # Replace with your actual product attributes
 
-    recommended_products = product_search(user_query)
+    recommended_products = word2vec_search(user_query)
 
     # Render a template or return the recommended products as a response
     return render_template('search_results.html', products=recommended_products)
